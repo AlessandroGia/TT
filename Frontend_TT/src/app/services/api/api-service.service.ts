597dba2e4efc860d8ccd843edd245c8bf11e18bc
AlessandroGia <alexgiacento@gmail.com>
2024-06-17 13:09:19 +0200

    fixes

--- Frontend_TT/src/app/services/api/api-service.service.ts
+++ Frontend_TT/src/app/services/api/api-service.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Config } from 'config-app';
-import config from '../../../../config-app.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  private _config: Config = config;
+  private config?: Config; 
 
-  constructor() { }
+  constructor() {
+  }
   
   // Root endpoint
-  public url = `http://${this._config.ip}:${this._config.port}/api/v1`;
+  public url = `http:///api/v1`;
   
 }
