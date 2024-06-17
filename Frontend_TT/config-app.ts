43c333a1aa968576ed2cfb3e121be5bf2ae2c8c3
AlessandroGia <alexgiacento@gmail.com>
2024-06-17 12:43:12 +0200

    changed ip and port binding

--- Frontend_TT/config-app.ts
+++ Frontend_TT/config-app.ts
@@ -1,5 +1,6 @@
 export interface Config
 {
     ip: string;
+    port: string;
 }
 
--- Frontend_TT/src/app/services/api/api-service.service.ts
+++ Frontend_TT/src/app/services/api/api-service.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from '@angular/common/http';
+import { Config } from 'config-app';
+import config from '../../../../config-app.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
+  private _config: Config = config;
+
   constructor() { }
   
   // Root endpoint
-  public url = "http://192.168.0.109:8080/api/v1";
+  public url = `http://${this._config.ip}:${this._config.port}/api/v1`;
   
 }
